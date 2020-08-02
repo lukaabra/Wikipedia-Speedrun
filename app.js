6b8a22dca82fb905e791283bfe41d0e4e5afea71
Luka Abramović <lukaabramovic2@gmail.com>
2020-08-02 13:13:38 +0200

    Starting to work on REST API with EJS as the view engine.

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-const seed = require('./seed.js')
+var express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    Article = require('./models/articles.js');
 
-const Article = require('./models/articles.js')
+// const seed = require('./seed.js')    // Database seeding
 
 
 //====================================================
 // MONGOOSE SETUP
 //====================================================
 
-mongoose.connect('mongodb://localhost/test', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// mongoose.connect('mongodb://localhost:27017/test', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
 
-
+// Database seeding
 // (async () => {
 //     seed.seedDb('Rijeka');
 // })();
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("landing");
+});
+
+app.listen(3000, () => {
+    console.log("Server starting at port 3000 ...");
+});
