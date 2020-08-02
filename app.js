31b980a4c5fdcb20bcc4c395981788a91734e77a
Luka Abramović <lukaabramovic2@gmail.com>
2020-08-02 18:55:34 +0200

    Made bare bones GET requests for: Index (landing page), instructions, and random article.

--- app.js
+++ app.js
@@ -10,20 +10,42 @@ var express = require('express'),
 // MONGOOSE SETUP
 //====================================================
 
-// mongoose.connect('mongodb://localhost:27017/test', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
+mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // Database seeding
 // (async () => {
 //     seed.seedDb('Rijeka');
 // })();
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
+// GET INDEX
 app.get("/", (req, res) => {
     res.render("landing");
+});
+
+// GET INSTRUCTIONS
+app.get("/instructions", (req, res) => {
+    res.render("instructions");
+});
+
+// GET RANDOM ARTICLE
+app.get("/start", (req, res) => {
+    Article.find({
+        title: "Ogulin"
+    }, (err, randomArticle) => {
+
+        console.log(randomArticle.length)
+
+        if (err) console.log(err);
+
+        else res.render("start", {
+            randomArticle: randomArticle
+        });
+    })
 });
 
 app.listen(3000, () => {
