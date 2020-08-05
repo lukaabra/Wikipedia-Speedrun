c01ae050678633f33c6ba0c0023359d2ec7c6ebf
Luka Abramović <lukaabramovic2@gmail.com>
2020-08-05 17:50:32 +0200

    Adding generation of random article from database.

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var express = require('express'),
 
 // const seed = require('./seed.js')    // Database seeding
 
+const ARTICLE_COUNT = 47163;
 
 //====================================================
 // MONGOOSE SETUP
 //====================================================
 
-mongoose.connect('mongodb://localhost:27017/test', {
+mongoose.connect('mongodb://localhost:27017/wiki_articles', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -34,18 +35,27 @@ app.get("/instructions", (req, res) => {
 
 // GET RANDOM ARTICLE
 app.get("/start", (req, res) => {
-    Article.find({
-        title: "Ogulin"
-    }, (err, randomArticle) => {
 
-        console.log(randomArticle.length)
+    let random = Math.floor(Math.random() * ARTICLE_COUNT);
 
+    // TODO: If the user refreshes the page lots of times, the skip(random) will skip outside of the database
+    // and return null as randomArticle
+    Article.findOne({
+        "links.1": {
+            "$exists": true
+        }
+    }).skip(random).exec((err, randomArticle) => {
+        try {
+            console.log(randomArticle.links.length)
+        } catch (e) {
+            if (e instanceof TypeError) console.log("TYPE ERROR: " + randomArticle)
+            else console.log(e)
+        }
         if (err) console.log(err);
-
         else res.render("start", {
             randomArticle: randomArticle
         });
-    })
+    });
 });
 
 app.listen(3000, () => {
