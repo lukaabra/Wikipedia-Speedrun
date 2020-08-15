1b8cb49f523ec40231ec43649077f7c220c78f4f
Luka Abramović <lukaabramovic2@gmail.com>
2020-08-15 14:22:00 +0200

    Implemented link connectivity on the website.

--- routes/play.js
+++ routes/play.js
@@ -1,16 +1,14 @@
 var express = require('express')
 var router = express.Router();
 var Article = require('../models/articles');
-var graph = require('../seed/graph.js');
 
-const ARTICLE_COUNT = 47163
 
 function generateRandomArticle() {
     // Returns an object inside of an array, wrapped in a Promise --> Promise( [{...}] )
 
     // Returns a Promise
     let randomArticle = Article.aggregate().match({
-        "links.1": { // Check if the record has at least 1 link
+        "edges.1": { // Check if the record has at least 1 link
             "$exists": true
         }
     }).sample(1).exec();
@@ -23,28 +21,32 @@ router.get("/start", async (req, res) => {
     const RANDOM_STARTING_ARTICLE = (await generateRandomArticle())[0];
 
     res.render("play/start", {
-        startingArticle: RANDOM_STARTING_ARTICLE
+        article: RANDOM_STARTING_ARTICLE
     });
 });
 
 // GET ARTICLE
 router.get("/play/:id", async (req, res) => {
 
-    Article.findById(req.params.id, async (err, foundArticle) => {
-        if (err) {
-            console.log("GET ARTICLE ERROR: " + err)
-        } else {
+    let currentArticle = await Article.findById(req.params.id, (err, foundArticle) => {
+        if (err) console.log("GET ARTICLE ERROR: " + err)
+    });
+    let currentArticleEdges = {};
 
-            let g = new graph.Graph(foundArticle);
-            await g.constructGraphToShow();
+    // Iterate over all the edges
+    for (let edge of currentArticle.edges) {
+        let edgeRecord = await Article.findOne({
+            'title': edge
+        }, (err, foundEdgeRecord) => {
+            if (err) console.log("GET ARTICLE EDGE ERROR: " + err)
+        });
 
-            console.log(Object.keys(g.nodes).length)
+        currentArticleEdges[edgeRecord.title] = edgeRecord
+    }
 
-            res.render("play/play", {
-                center: g.center,
-                children: g.nodes
-            });
-        }
+    res.render('play/show', {
+        article: currentArticle,
+        currentArticleEdges
     });
 });
 
