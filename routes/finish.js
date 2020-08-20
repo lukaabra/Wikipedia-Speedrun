54e9620752b68cb2c77b51555d4d95c96558bb23
Luka Abramović <lukaabramovic2@gmail.com>
2020-08-20 17:11:53 +0200

    Added surrender form to the show route for each article. Upon submitting the surrender form, the user is taken to the finishing screen, and his/her score is re-set to zero.

--- routes/finish.js
+++ routes/finish.js
@@ -4,12 +4,21 @@ var router = express.Router();
 
 // GET INDEX
 router.get("/finish", (req, res) => {
+    // If the user surrendered, reset the score to 0
+    if (req.query.surrender) {
+        req.session.userScore = 0;
+        req.session.userPath.push('SURRENDERED!');
+    } else {
+        req.session.userPath.push('Rijeka');
+    };
+
     res.render("finish", {
         shortestPath: req.session.shortestPath,
         bestPossibleScore: req.session.bestPossibleScore,
         startingArticle: req.session.startingArticle,
         userScore: req.session.userScore,
-        userPath: req.session.userPath
+        userPath: req.session.userPath,
+        surrender: req.query.surrender
     });
 });
 
