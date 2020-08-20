c36be01867f930b0cd57b694264fa6cd8b46bbee
Luka Abramović <lukaabramovic2@gmail.com>
2020-08-20 17:54:46 +0200

    Reorganizing code.

--- routes/finish.js
+++ routes/finish.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router();
 
 
+// https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
 function millisToMinutesAndSeconds(millis) {
     let minutes = Math.floor(millis / 60000);
     let seconds = ((millis % 60000) / 1000).toFixed(0);
@@ -14,6 +15,14 @@ router.get("/finish", (req, res) => {
     // Time the user finished the run
     req.session.finishingTime = Date.now();
 
+    // Set the elapsed time of the run
+    let timeElapsed = req.session.finishingTime - req.session.startingTime;
+    if (timeElapsed >= 3600000) {
+        req.session.totalRunTime = 'Over 1 hour'
+    } else {
+        req.session.totalRunTime = millisToMinutesAndSeconds(timeElapsed);
+    }
+
     // If the user surrendered, reset the score to 0
     if (req.query.surrender) {
         req.session.userScore = 0;
@@ -21,15 +30,6 @@ router.get("/finish", (req, res) => {
     } else {
         req.session.userPath.push('Rijeka');
     };
-
-    // Set the elapsed time of the run
-    let timeElapsed = req.session.finishingTime - req.session.startingTime;
-    if (timeElapsed >= 3600000) {
-        req.session.totalRunTime = 'Over 1 hour'
-    } else {
-        // https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
-        req.session.totalRunTime = millisToMinutesAndSeconds(timeElapsed);
-    }
 
     res.render("finish", {
         shortestPath: req.session.shortestPath,
