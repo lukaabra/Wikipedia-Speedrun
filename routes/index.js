5f58fba747f293e2b93161cd314d4494b49ce590
Luka Abramović <lukaabramovic2@gmail.com>
2020-08-21 16:18:18 +0200

    Implemented retrieval of top 3 scores on the landing page.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express')
 var router = express.Router();
+var Score = require('../models/scores');
 
 
 // GET INDEX
-router.get("/", (req, res) => {
-    res.render("landing");
+router.get("/", async (req, res) => {
+    // Retrieve top 3 scores
+    let topThreeScores = await Score.aggregate().sort({
+        'score': 'asc'
+    }).limit(3).exec();
+
+    res.render("landing", {
+        scores: topThreeScores
+    });
 });
 
 module.exports = router;
