2159bbd8fdeec787ba21c8ca52127232f9bdf75d
Luka Abramović <lukaabramovic2@gmail.com>
2020-08-24 14:08:06 +0200

    Refactored code putting it into a lot of smaller functions.

--- routes/finish.js
+++ routes/finish.js
@@ -2,18 +2,13 @@ var express = require('express');
 var router = express.Router();
 var Score = require('../models/scores');
 
-const MAX_ALLOWED_RANK = 3;
+const MAX_ALLOWED_RANK = 3,
+    RANK_TABLE_SIZE = 100;
 
 
-// https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
-function millisToMinutesAndSeconds(millis) {
-    let minutes = Math.floor(millis / 60000);
-    let seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-}
-
-
-// GET FINISH
+//==================================================
+//              GET FINISH ROUTE
+//==================================================
 router.get("/finish", (req, res) => {
 
     // If the user surrendered, reset the score to 0
@@ -46,35 +41,27 @@ router.get("/finish", (req, res) => {
 });
 
 
-// POST FINISH
+//==================================================
+//              POST FINISH ROUTE
+//==================================================
 router.post("/finish", async (req, res) => {
+    // Save the users submitted name to the session
     req.session.name = req.body.name;
 
-    // Time the user finished the run
     req.session.finishingTime = Date.now();
+    let runTimeInMs = req.session.finishingTime - req.session.startingTime;
+    req.session.totalRunTime = setElapsedRunTime(runTimeInMs);
 
-    // Set the elapsed time of the run
-    let timeElapsed = req.session.finishingTime - req.session.startingTime; // in ms
-    if (timeElapsed >= 3600000) {
-        req.session.totalRunTime = 'Over 1 hour'
-    } else {
-        req.session.totalRunTime = millisToMinutesAndSeconds(timeElapsed);
-    }
+    req.session.score = calculateScore(runTimeInMs, req.session.userSteps);
 
-    // Calculate total score of the run
-    let coefficient = Math.floor((timeElapsed / 1000) / req.session.userSteps)
-    let calculatedScore = Math.floor((timeElapsed / 1000) * req.session.userSteps + coefficient);
-    req.session.score = calculatedScore;
-
+    // Retrieve the whole ranking table
     let completeRankingTable = await Score.aggregate().sort({
         'score': 'asc'
-    }).limit(100).exec();
+    }).limit(RANK_TABLE_SIZE).exec();
 
-    var rank = calculateRanking(calculatedScore, completeRankingTable);
+    var rank = calculateRanking(req.session.score, completeRankingTable);
 
     if (rank <= MAX_ALLOWED_RANK) {
-        console.log("===================");
-        console.log("RANK: " + rank);
         await updateRankingTable(rank, completeRankingTable);
         await deleteSurplusScores();
 
@@ -85,14 +72,13 @@ router.post("/finish", async (req, res) => {
             steps: req.session.userSteps,
             minPossibleSteps: req.session.minPossibleSteps,
             startingArticle: req.session.startingArticle,
-            score: calculatedScore,
+            score: req.session.score,
             rank: rank
         };
 
         await Score.create(scoreToSubmit, (err, submittedScore) => {
             if (err) console.log("SUBMITTING SCORE ERROR: " + err);
             else {
-                console.log("CREATED " + submittedScore.name + "'s score " + submittedScore.score + " at rank " + submittedScore.rank)
                 // Save the id of the submitted score so the ranking of the player can be shown on the finishing screen
                 req.session.rank = submittedScore.rank;
                 res.redirect("/finish");
@@ -103,6 +89,9 @@ router.post("/finish", async (req, res) => {
     }
 });
 
+//==================================================
+//              HELPER FUNCTIONS
+//==================================================
 function calculateRanking(playerScore, rankingTable) {
     // Check if array is empty (any ranks at all): 
     // https://stackoverflow.com/questions/24403732/how-to-check-if-array-is-empty-or-does-not-exist
@@ -113,17 +102,54 @@ function calculateRanking(playerScore, rankingTable) {
 
         while (low < high) {
             let mid = Math.floor((low + high) / 2);
-            if (rankingTable[mid].score < playerScore) {
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
+
+            if (rankingTable[mid].score < playerScore) low = mid + 1;
+            else high = mid;
         }
         // low is the index where the playerScore should be in rankingTable. Adding 1 will give the real rank in the table
         return low + 1
     } else {
         return 1
     }
+};
+
+function calculateScore(timeElapsed, userSteps) {
+    let coefficient = Math.floor((timeElapsed / 1000) / userSteps);
+    let calculatedScore = Math.floor((timeElapsed / 1000) * userSteps + coefficient);
+
+    return calculatedScore;
+}
+
+async function deleteSurplusScores() {
+    let scoresToDelete = await Score.aggregate().match({
+        "rank": {
+            "$gte": MAX_ALLOWED_RANK
+        }
+    }).exec();
+
+    for (let score of scoresToDelete) {
+        Score.findByIdAndDelete(score._id, (err, deletedScore) => {
+            if (err) console.log("DELETING EXTRA SCORE ERROR: " + err);
+        });
+    };
+};
+
+// https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
+function millisToMinutesAndSeconds(millis) {
+    let minutes = Math.floor(millis / 60000);
+    let seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+function setElapsedRunTime(runTimeInMs) {
+    let totalRunTime;
+    if (runTimeInMs >= 3600000) {
+        totalRunTime = 'Over 1 hour';
+    } else {
+        totalRunTime = millisToMinutesAndSeconds(runTimeInMs);
+    }
+
+    return totalRunTime;
 };
 
 async function updateRankingTable(rankFromWhichToUpdate, rankingTable) {
@@ -136,24 +162,8 @@ async function updateRankingTable(rankFromWhichToUpdate, rankingTable) {
             }
         }, (err, foundScore) => {
             if (err) console.log("UPDATE RANK ERROR: " + err);
-            else console.log("UPDATED " + foundScore.name + "'s rank " + foundScore.rank + " to " + Number(foundScore.rank + 1).toString());
         });
     }
 };
-
-async function deleteSurplusScores() {
-    let scoresToDelete = await Score.aggregate().match({
-        "rank": {
-            "$gte": MAX_ALLOWED_RANK
-        }
-    }).exec();
-
-    for (let score of scoresToDelete) {
-        Score.findByIdAndDelete(score._id, (err, deletedScore) => {
-            if (err) console.log("DELETING EXTRA SCORE ERROR: " + err);
-            else console.log(deletedScore.name + "'s rank " + deletedScore.rank + " deleted successfully!")
-        });
-    };
-}
 
 module.exports = router;
