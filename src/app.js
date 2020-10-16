8f6655a531c890357460f40f6eca69d3c898718d
Luka Abramović <lukaabramovic2@gmail.com>
2020-10-16 02:05:42 +0000

    Added webpack configuration and required files and folders.

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsx = (
+    <div>
+        Test
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public', 'dist'), // absolute path
+            filename: 'bundle.js'
+        },
+        mode: 'development',
+        module: {
+            rules: [{
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime', '@babel/plugin-proposal-object-rest-spread']
+                    }
+                }
+            }, {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }]
+            }]
+        },
+        plugins: [new MiniCssExtractPlugin()],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'), // absolute path
+            port: 3000,
+            historyApiFallback: true,
+            publicPath: '/dist/'
+        }
+    }
+};
