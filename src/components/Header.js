677473b8c85701591cf8681e6fe0d287a99ec5b8
Luka Abramović <lukaabramovic2@gmail.com>
2020-10-30 12:51:56 +0100

    Finished styling header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 const Header = (props) => (
     <div className="header">
-        <NavLink to={"/"} className="header__title">
-            <h1>Wikipedia Speedrun</h1>
+        <NavLink to={"/"} className="header__link">
+            <h1 className="header__link__title">Wikipedia Speedrun</h1>
         </NavLink>
 
         {props.subtitle && <h3 className="header__subtitle">{props.subtitle}</h3>}
